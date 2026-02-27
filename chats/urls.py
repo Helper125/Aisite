98cd140361402e8595a1dd975730819843cf55e5
Dmytro Ridchenko <dimaridcenko269@gmail.com>
2026-02-27 23:22:33 +0200

    Update urls.py

--- chats/urls.py
+++ chats/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
     path("speak_message<int:message_id>/", speak_messagge, name="speak_message")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
