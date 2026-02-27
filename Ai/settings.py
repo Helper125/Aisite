4210fba74d36cec4fad73874d70d99373ebccd0c
Dmytro Ridchenko <dimaridcenko269@gmail.com>
2026-02-27 22:52:11 +0200

    all recreated repositorion

--- Ai/settings.py
+++ Ai/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2#h4&yt(qj*y+*5+4j33xt^#syju((c7%+5d$gtv#3j--vvkha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# CSRF_COOKIE_SECURE = True          # обов’язково для HTTPS
-# CSRF_TRUSTED_ORIGINS = ["https://aisite-production.up.railway.app"]
-# SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True          # обов’язково для HTTPS
+CSRF_TRUSTED_ORIGINS = ["https://aisite-production.up.railway.app"]
+SESSION_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
